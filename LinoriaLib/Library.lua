dda7ec6616c2a8141dfa1ed2f444004ca59d40cb
Songkranz <86290275+Songkranz@users.noreply.github.com>
2022-12-31 09:40:37 +0700

    Update Library.lua

--- LinoriaLib/Library.lua
+++ LinoriaLib/Library.lua
@@ -2159,7 +2159,7 @@ do
 
     local WatermarkOuter = Library:Create('Frame', {
         BorderColor3 = Color3.new(0, 0, 0);
-        Position = UDim2.new(0, 252, 0, -280);
+        Position = UDim2.new(0, 256, 0, -28);
         Size = UDim2.new(0, 213, 0, 20);
         ZIndex = 200;
         Visible = false;
